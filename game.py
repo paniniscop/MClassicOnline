76f470646a0eac85c384396a85fa988fead118cb
paniniscop <137611446+paniniscop@users.noreply.github.com>
2023-08-11 18:48:02 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
 # game.py
+# read readme.md on my github 
+from selenium import webdriver
+
+
+driver_path = "chemin/vers/le/driver/chromedriver"
+
+
+driver = webdriver.Chrome(driver_path)
+
+
+url = "https://classic.minecraft.net/?join=vqcrKCVOytfy93qtnec"
+
+driver.get(url)
+
+
+input("Appuyez sur Entrée pour quitter...")
+
+
+driver.quit()
